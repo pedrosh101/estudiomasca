340ac816c52e6ddd5324d3266cb51ff38d8c4b74
pedrosh101 <pedro.sh101@gmail.com>
2024-02-19 15:18:54 -0300

    colorshome

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,12 @@ import Image from "next/image";
 import Fundo from "../../public/img/prov.jpg";
 import Asset1 from "../../public/img/Asset1.png";
 import Asset2 from "../../public/img/Asset2.png";
-import Asset6 from "../../public/img/Asset6.png";
-import Asset8 from "../../public/img/Asset8.png";
+import Asset3 from "../../public/img/Asset3.png";
 import Asset5 from "../../public/img/Asset5.png";
-import Logo from "../../public/img/logo.png";
+import Asset6 from "../../public/img/Asset6.png";
+import Asset7 from "../../public/img/Asset7.png";
+import Asset8 from "../../public/img/Asset8.png";
+import Logo from "../../public/img/logoblack.png";
 import Navbar from "./components/navbar";
 
 export default function Home() {
@@ -20,7 +22,9 @@ export default function Home() {
         if (prevImage === Asset1) return Asset2;
         if (prevImage === Asset2) return Asset6;
         if (prevImage === Asset6) return Asset8;
-        if (prevImage === Asset8) return Asset5;
+        if (prevImage === Asset8) return Asset3;
+        if (prevImage === Asset3) return Asset7;
+        if (prevImage === Asset7) return Asset5;
         if (prevImage === Asset5) return Logo;
         if (prevImage === Logo) return Logo;
       });
@@ -30,8 +34,8 @@ export default function Home() {
   }, []);
 
   return (
-    <main className="flex min-h-screen flex-col items-center bg-slate-900 justify-between">
-      <Navbar isProjetosPage={false} />
+    <main className="flex min-h-screen flex-col items-center bg-clr1 justify-between">
+      <Navbar isProjetosPage={true} />
       {/* <Image
         src={Fundo}
         alt="Imagem"
